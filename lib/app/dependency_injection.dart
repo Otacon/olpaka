a267ca6b2dce8fd4650714138be070de4c668048
Orfeo Ciano <orfeo18@gmail.com>
2024-04-27 12:33:31 +0100

    Initial commit (#1)
    initial project setup

---
+++ lib/app/dependency_injection.dart
@@ -0,0 +1,7 @@
+
+import 'package:get_it/get_it.dart';
+
+void registerModules() {
+  var l = GetIt.instance;
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:olpaka/generated/l10n.dart';
+import 'package:olpaka/app/dependency_injection.dart';
+import 'package:olpaka/app/olpaka_app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  registerModules();
+  await _configureLocalization();
+  runApp(const OlpakaApp());
+}
+
+
+_configureLocalization() async {
+  await S.load(const Locale("en"));
+}
