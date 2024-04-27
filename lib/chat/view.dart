0a545aaec7860a0352695e4a72134bcc9c740c41
Orfeo <orfeo18@gmail.com>
2024-04-27 21:50:51 +0100

    Added some UI.

--- lib/chat/view.dart
+++ lib/chat/view.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get_it/get_it.dart';
+import 'package:markdown_widget/markdown_widget.dart';
 import 'package:olpaka/chat/view_model.dart';
 import 'package:stacked/stacked.dart';
 
@@ -14,16 +17,136 @@ class ChatScreen extends StatelessWidget {
         viewModel.onCreate();
       },
       builder: (context, viewModel, child) {
+        final state = viewModel.state;
         return Scaffold(
-          appBar: AppBar(
-            title: const Text("Olpaka"),
-            backgroundColor: Theme
-                .of(context)
-                .colorScheme
-                .inversePrimary,
-          ),
-        );
+            appBar: AppBar(
+              title: const Text("Olpaka"),
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            body: _Content(
+              messages: state.messages,
+              models: state.models,
+              selectedModel: state.selectedModel,
+            ));
       },
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+  const _Content({
+    required this.messages,
+    required this.models,
+    this.selectedModel,
+  });
+
+  final List<ChatMessage> messages;
+  final List<String> models;
+  final String? selectedModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: messages.length,
+            itemBuilder: (context, index) {
+              final message = messages[index];
+              final double paddingStart;
+              final double paddingEnd;
+              if (message.isUser) {
+                paddingStart = 48;
+                paddingEnd = 16;
+              } else {
+                paddingStart = 16;
+                paddingEnd = 48;
+              }
+              return Padding(
+                padding: EdgeInsets.only(
+                    left: paddingStart, right: paddingEnd, top: 16.0),
+                child: Card.filled(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 16.0,
+                    ),
+                    child: _Message(
+                      isOwn: message.isUser,
+                      text: message.message,
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      onPressed: () => {},
+                      icon: const Icon(Icons.send),
+                    ),
+                    border: const OutlineInputBorder(),
+                    hintText: "Message Olpaka",
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16.0),
+              DropdownMenu<String>(
+                width: 200,
+                label: const Text("Model"),
+                initialSelection: selectedModel,
+                dropdownMenuEntries: models
+                    .map((e) => DropdownMenuEntry(value: e, label: e))
+                    .toList(),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _Message extends StatelessWidget {
+  const _Message({required this.isOwn, required this.text});
+
+  final bool isOwn;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    final Text sender;
+    final CrossAxisAlignment alignment;
+    if (isOwn) {
+      alignment = CrossAxisAlignment.start;
+      sender = Text(
+        "You",
+        style: Theme.of(context).textTheme.titleLarge,
+      );
+    } else {
+      alignment = CrossAxisAlignment.end;
+      sender = Text(
+        "Assistant",
+        style: Theme.of(context).textTheme.titleLarge,
+      );
+    }
+    return Column(
+      crossAxisAlignment: alignment,
+      children: [
+        sender,
+        MarkdownBlock(
+          data: text,
+          selectable: true,
+        )
+      ],
     );
   }
 }
--- lib/chat/view_model.dart
+++ lib/chat/view_model.dart
@@ -1,18 +1,69 @@
-
 import 'package:flutter/foundation.dart';
 import 'package:olpaka/ollama/repository.dart';
 
 class ChatViewModel with ChangeNotifier {
   final OllamaRepository _repository;
 
+  ChatState state = ChatState(
+    isLoading: true,
+    selectedModel: null,
+    models: List.empty(),
+    messages: List.empty(),
+  );
+
   ChatViewModel(this._repository);
 
   onCreate() async {
-    final models = await _repository.listModels();
-    final modelName = models.first.name;
-    const prompt = "Why is the sky blue?";
-    print("Generating answer with $modelName: $prompt\n");
-    final response = await _repository.generate(models.first.name, prompt);
-    print(response);
+    final models =
+        (await _repository.listModels()).map((model) => model.name).toList();
+
+    final selectedModel = models.firstOrNull;
+
+    state = ChatState(
+      isLoading: false,
+      selectedModel: selectedModel,
+      models: models,
+      messages: List.empty(),
+    );
+    notifyListeners();
+    const prompt = "Could you please generate a message with all the formattings of markdown?!";
+    final ownMessage = ChatMessage(true, prompt);
+    state = ChatState(
+      isLoading: false,
+      selectedModel: selectedModel,
+      models: models,
+      messages: [ownMessage],
+    );
+    notifyListeners();
+    final answer = await _repository.generate(selectedModel!, prompt);
+    final reply = ChatMessage(false,answer);
+    state = ChatState(
+      isLoading: false,
+      selectedModel: selectedModel,
+      models: models,
+      messages: [ownMessage, reply],
+    );
+    notifyListeners();
   }
+}
+
+class ChatState {
+  final bool isLoading;
+  final String? selectedModel;
+  final List<String> models;
+  final List<ChatMessage> messages;
+
+  ChatState({
+    required this.isLoading,
+    required this.selectedModel,
+    required this.models,
+    required this.messages,
+  });
+}
+
+class ChatMessage {
+  final bool isUser;
+  final String message;
+
+  ChatMessage(this.isUser, this.message);
 }
