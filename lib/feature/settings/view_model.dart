9019d61ea0dc4388af6c67dbb46210feceb76df9
Orfeo Ciano <orfeo18@gmail.com>
2024-05-14 07:14:20 +0100

    Added about dialog (#14)

--- lib/feature/settings/view_model.dart
+++ lib/feature/settings/view_model.dart
@@ -1,6 +1,7 @@
 import 'package:olpaka/core/state/theme/theme_domain.dart';
 import 'package:olpaka/core/state/theme/theme_state_holder.dart';
 import 'package:olpaka/feature/settings/state.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:stacked/stacked.dart';
 
 class SettingsViewModel extends BaseViewModel {
@@ -13,18 +14,25 @@ class SettingsViewModel extends BaseViewModel {
     state = SettingsState(
       _themeManager.themeMode.value,
       _themeManager.themeColor.value,
+      await _appVersion(),
     );
+    notifyListeners();
   }
 
   onThemeModeChanged(OlpakaThemeMode themeMode) async {
     _themeManager.setThemeMode(themeMode);
-    state = SettingsState(themeMode, state.themeColor);
+    state = SettingsState(themeMode, state.themeColor, state.appVersion);
     notifyListeners();
   }
 
   onSeedColorChanged(OlpakaThemeColor color) async {
     _themeManager.setThemeColor(color);
-    state = SettingsState(state.themeMode, color);
+    state = SettingsState(state.themeMode, color, state.appVersion);
     notifyListeners();
+  }
+
+  Future<String> _appVersion() async {
+    final packageInfo = await PackageInfo.fromPlatform();
+    return "${packageInfo.appName} v${packageInfo.version}";
   }
 }
