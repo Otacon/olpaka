b2be8b05060d50cb1707923fe304c2831b5a734d
Orfeo <orfeo18@gmail.com>
2024-05-21 09:20:06 +0100

    Adds toString to events

--- lib/core/analytics/event.dart
+++ lib/core/analytics/event.dart
@@ -6,28 +6,53 @@ class EventDownloadModel extends Event {
   String modelName;
 
   EventDownloadModel(this.modelName);
+
+  @override
+  String toString() {
+    return 'EventDownloadModel{modelName: $modelName}';
+  }
 }
 
 class EventRemoveModel extends Event {
   String modelName;
 
   EventRemoveModel(this.modelName);
+
+  @override
+  String toString() {
+    return 'EventRemoveModel{modelName: $modelName}';
+  }
 }
 
 class EventSendMessage extends Event {
   String modelName;
 
   EventSendMessage(this.modelName);
+
+  @override
+  String toString() {
+    return 'EventSendMessage{modelName: $modelName}';
+  }
 }
 
 class EventChangeThemeMode extends Event{
   OlpakaThemeMode themeMode;
 
   EventChangeThemeMode(this.themeMode);
+
+  @override
+  String toString() {
+    return 'EventChangeThemeMode{themeMode: $themeMode}';
+  }
 }
 
 class EventChangeThemeColor extends Event{
   OlpakaThemeColor themeColor;
 
   EventChangeThemeColor(this.themeColor);
+
+  @override
+  String toString() {
+    return 'EventChangeThemeColor{themeColor: $themeColor}';
+  }
 }
