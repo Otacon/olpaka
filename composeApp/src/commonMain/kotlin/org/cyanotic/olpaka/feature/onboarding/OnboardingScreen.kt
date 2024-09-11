278a59dbcace93f578fc4fd82b874394b5e7cd7f
Orfeo <orfeo18@gmail.com>
2024-09-11 08:58:19 +0100

    Fixed dialog dimming.

--- composeApp/src/commonMain/kotlin/org/cyanotic/olpaka/feature/onboarding/OnboardingScreen.kt
+++ composeApp/src/commonMain/kotlin/org/cyanotic/olpaka/feature/onboarding/OnboardingScreen.kt
@@ -9,8 +9,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavHostController
 import olpaka.composeapp.generated.resources.*
 import org.jetbrains.compose.resources.stringResource
@@ -26,70 +24,63 @@ fun OnboardingScreen(navController: NavHostController) {
     LaunchedEffect(Unit) {
         viewModel.onCreate()
         viewModel.event.collect { event ->
-            when(event){
+            when (event) {
                 OnboardingEvent.Close -> navController.popBackStack()
                 is OnboardingEvent.OpenBrowser -> Unit
             }
         }
     }
-    Dialog(
-        onDismissRequest = { navController.popBackStack() },
-        properties = DialogProperties(
-            dismissOnBackPress = true,
-            dismissOnClickOutside = true,
-        )
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        shape = AlertDialogDefaults.shape,
+        color = AlertDialogDefaults.containerColor,
+        tonalElevation = AlertDialogDefaults.TonalElevation
     ) {
-        Surface(
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight(),
-            shape = AlertDialogDefaults.shape,
-            color = AlertDialogDefaults.containerColor,
-            tonalElevation = AlertDialogDefaults.TonalElevation
+        Column(
+            modifier = Modifier.padding(24.dp)
         ) {
+            Text(
+                text = stringResource(Res.string.onboarding_title),
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            // Custom Content (replace with your custom layout)
             Column(
-                modifier = Modifier.padding(24.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Top
             ) {
-                Text(
-                    text = stringResource(Res.string.onboarding_title),
-                    style = MaterialTheme.typography.headlineSmall,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-
-                // Custom Content (replace with your custom layout)
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 16.dp),
-                    horizontalAlignment = Alignment.Start,
-                    verticalArrangement = Arrangement.Top
-                ) {
-                    when (state.currentStep) {
-                        0 -> StepOne()
-                        1 -> StepTwo()
-                        2 -> StepThree()
-                        else -> StepThree()
-                    }
+                when (state.currentStep) {
+                    0 -> StepOne()
+                    1 -> StepTwo()
+                    2 -> StepThree()
+                    else -> StepThree()
                 }
+            }
 
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End
-                ) {
-                    if(state.isPreviousVisible) {
-                        TextButton(
-                            onClick = viewModel::onPreviousPressed,
-                        ) {
-                            Text(stringResource(Res.string.onboarding_cta_previous))
-                        }
-                        Spacer(modifier = Modifier.width(8.dp))
-                    }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                if (state.isPreviousVisible) {
                     TextButton(
-                        onClick = viewModel::onNextPressed
+                        onClick = viewModel::onPreviousPressed,
                     ) {
-                        Text(state.nextText)
+                        Text(stringResource(Res.string.onboarding_cta_previous))
                     }
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+                TextButton(
+                    onClick = viewModel::onNextPressed
+                ) {
+                    Text(state.nextText)
                 }
             }
         }
