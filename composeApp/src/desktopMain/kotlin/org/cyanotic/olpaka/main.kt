bf87b81121a95296932eaf7dce18c775f1d79ae0
Orfeo <orfeo18@gmail.com>
2024-09-15 08:10:38 +0100

    Added icon for desktop.

--- composeApp/src/desktopMain/kotlin/org/cyanotic/olpaka/main.kt
+++ composeApp/src/desktopMain/kotlin/org/cyanotic/olpaka/main.kt
@@ -4,12 +4,16 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import olpaka.composeapp.generated.resources.Res
+import olpaka.composeapp.generated.resources.desktop_icon
+import org.jetbrains.compose.resources.painterResource
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
+        icon = painterResource(Res.drawable.desktop_icon),
         title = "Olpaka",
-        state = WindowState(width = 1024.dp, height = 768.dp)
+        state = WindowState(width = 1024.dp, height = 768.dp),
     ) {
         App()
     }
