156164f59ad016036ea5fe9c53e5eaa219067602
Orfeo <orfeo18@gmail.com>
2024-11-10 11:10:29 +0000

    Configure Bugsnag Desktop

--- composeApp/src/desktopMain/kotlin/org/cyanotic/olpaka/main.kt
+++ composeApp/src/desktopMain/kotlin/org/cyanotic/olpaka/main.kt
@@ -4,11 +4,18 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import com.bugsnag.Bugsnag
+import com.cyanotic.olpaka.BuildKonfig
 import olpaka.composeapp.generated.resources.Res
 import olpaka.composeapp.generated.resources.desktop_icon
 import org.jetbrains.compose.resources.painterResource
 
 fun main() = application {
+    if(BuildKonfig.bugsnagApiKey.isNotBlank()) {
+        val bugsnag = Bugsnag(BuildKonfig.bugsnagApiKey)
+        bugsnag.setAppVersion(BuildKonfig.appVersion)
+        bugsnag.setReleaseStage(BuildKonfig.appVariant)
+    }
     Window(
         onCloseRequest = ::exitApplication,
         icon = painterResource(Res.drawable.desktop_icon),
